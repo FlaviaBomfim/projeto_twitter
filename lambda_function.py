9454cb450f67d4c14bb5925dab1921a55e2b126c
Flavia Bomfim <flavia.bomfim@icloud.com>
2018-12-28 17:58:41 +0000

    Lambda

---
+++ lambda_function.py
@@ -0,0 +1,16 @@
+from TwitterAPI import TwitterAPI
+import boto3
+import json
+
+## twitter credentials
+
+consumer_key = "xxxxxxxxxxxxxxxxxxxxxxx"
+consumer_secret = "xxxxxxxxxxxxxxxxxxxxxxx"
+access_token_key = "xxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxx"
+access_token_secret = "xxxxxxxxxxxxxxxxxxxxxxx"
+
+
+def lambda_handler(event, context):
+        r = event.api.GetSearch('search/tweets', {'q':'JORGINHO'})
+            for item in r:
+                        return(item)
